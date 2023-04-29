5c56fe875cf44c5fa8f0a1336911bea7cda15f57
a-horse-with-no-name <alyabra.vargas@gmail.com>
2023-04-29 12:49:37 -0600

    Agregar columnas de rol y enrollment a user,  agregar los campos al formulario de register y creacion del modelo, controllador y migracion de clase, y cambio del idioma a es

---
+++ database/migrations/2023_04_29_172521_add_rol_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('rol'); //1= admin, 2=teacher, 3=student, 0=superadmin
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('rol');
+        });
+    }
+};
